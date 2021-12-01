ccb74351be2811fb5bcbb9b31fb2501eed917ddb
yangqian3 <yangqian3@kingsoft.com>
2021-12-01 11:33:04 +0800

    middleware

--- __json_server_mock__/middleware.js
+++ __json_server_mock__/middleware.js
@@ -1,5 +1,6 @@
 module.exports = (req,res,next) => {
-    if(req.method == 'POST' && req.path === '/login'){
+    console.log('....middleware')
+    if(req.method ===  'POST' && req.path === '/login'){
         if(req.body.username === 'yq' && req.body.password === '123456'){
             return res.status(200).json({
                 user:{
